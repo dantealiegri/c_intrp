5159fa7646d2b0af76e89f02b588412368e9e800
Drew Ogle <dogle@orbit.(none)>
2009-05-01 09:58:09 -0400

    fixed

--- main.cc
+++ main.cc
@@ -2,4 +2,15 @@
 
 int main( int argc , char ** argv )
 {
+	int pret;
+	if( ( pret= parse_args( argc, argv )) != 0 ) exit( pret );
+
+	read_input();
+
+	exit( 0 );
+}
+
+int parse_args( int argc , char ** argv )
+{
+	return 0;
 }
